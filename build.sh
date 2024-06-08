1c1e9b450a4bb0e79360ee12b83533c7fa5311cd
tremlab <rbalmert@gmail.com>
2024-06-08 16:33:56 -0700

    add render.yaml

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python od_job/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python od_job/manage.py migrate
